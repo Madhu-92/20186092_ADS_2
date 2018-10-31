df81d5d1029e9c1abf068e29ae300fce9e221c08
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-31 17:41:19 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2-Assignments/m3/3.1 Directed Cycle/Solution.java
@@ -3,33 +3,33 @@ import java.util.Scanner;
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		// Blank constructor.
-	}
-	/**
-	 * main method for performing operations.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int numOfVertices = Integer.parseInt(sc.nextLine());
-		int numOfEdges = Integer.parseInt(sc.nextLine());
-		DiGraph dgObj = new DiGraph(numOfVertices);
-		while (numOfEdges > 0) {
-			String[] connectedVertices = sc.nextLine().split(" ");
-			dgObj.addEdge(Integer.parseInt(connectedVertices[0]),
-			              Integer.parseInt(connectedVertices[1]));
-			numOfEdges--;
-		}
-		DirectedCycle dcObj = new DirectedCycle(dgObj);
-		if (dcObj.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+        // Blank constructor.
+    }
+    /**
+     * main method for performing operations.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfVertices = Integer.parseInt(sc.nextLine());
+        int numOfEdges = Integer.parseInt(sc.nextLine());
+        DiGraph dgObj = new DiGraph(numOfVertices);
+        while (numOfEdges > 0) {
+            String[] connectedVertices = sc.nextLine().split(" ");
+            dgObj.addEdge(Integer.parseInt(connectedVertices[0]),
+                          Integer.parseInt(connectedVertices[1]));
+            numOfEdges--;
+        }
+        DirectedCycle dcObj = new DirectedCycle(dgObj);
+        if (dcObj.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
