306e3a0db001b3b66cc88dbcc0bcdc59b1274b9c
Madhu-92 <madhu.ktej@msitprogram.net>
2018-11-04 23:03:17 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-Assignments/m1/1.1 Graph/Solution.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+/**
+ * Class for list graph.
+ */
+class ListGraph {
+    private int vertices;
+    private int edges;
+    private Bag<Integer>[] adjacent;
+    private String[] tokens;
+    ListGraph() {
+
+    }
+    ListGraph(final Scanner sc) {
+        this.vertices = Integer.parseInt(sc.nextLine());
+        adjacent = (Bag<Integer>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacent[i] = new Bag<Integer>();
+        }
+        int edge = Integer.parseInt(sc.nextLine());
+        tokens = sc.nextLine().split(",");
+        for (int i = 0; i < edge; i++) {
+            String[] inputs = sc.nextLine().split(" ");
+            addEdge(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]));
+        }
+    }
+
+    public void addEdge(final int vertexOne, final int vertexTwo) {
+        if (vertexOne == vertexTwo) {
+            return;
+        }
+        if (!hasEdge(vertexOne, vertexTwo)) {
+            edges++;
+        }
+        adjacent[vertexOne].add(vertexTwo);
+        adjacent[vertexTwo].add(vertexOne);
+    }
+
+    public Iterable<Integer> adj(final int vertex) {
+        return adjacent[vertex];
+    }
+    
+    public boolean hasEdge(final int vertexOne,
+                           final int vertexTwo) {
+        for (int each : adj(vertexOne))  {
+            if (each == vertexTwo) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append(vertices + " vertices, " + edges + " edges" + "\n");
+        if (edges > 0) {
+            for (int i = 0; i < vertices; i++) {
+                str.append(tokens[i] + ": ");
+                for (int j : adjacent[i]) {
+                    str.append(tokens[j] + " ");
+                }
+                str.append("\n");
+            }
+            return str.toString();
+        } else {
+            str.append("No edges");
+            return str.toString();
+        }
+    }
+}
+
+class MatrixGraph {
+    private String[] tokens;
+    private int[][] graphMatrix;
+    private int vertices;
+    private int edges;
+    MatrixGraph() {
+
+    }
+    MatrixGraph(final Scanner sc) {
+        this.vertices = Integer.parseInt(sc.nextLine());
+        graphMatrix = new int[vertices][vertices];
+        int edge = Integer.parseInt(sc.nextLine());
+        tokens = sc.nextLine().split(",");
+        for (int i = 0; i < edge; i++) {
+            String[] inputs = sc.nextLine().split(" ");
+            addEdge(Integer.parseInt(
+                        inputs[0]), Integer.parseInt(inputs[1]));
+        }
+
+    }
+    public void addEdge(final int vertexOne, final int vertexTwo) {
+        if (vertexOne != vertexTwo) {
+            if (!hasEdge(vertexOne, vertexTwo)) {
+                graphMatrix[vertexOne][vertexTwo] = 1;
+                graphMatrix[vertexTwo][vertexOne] = 1;
+                edges++;
+            }
+        }
+
+    }
+    public boolean hasEdge(final int vertexOne, final int vertexTwo) {
+        return graphMatrix[vertexOne][vertexTwo] == 1;
+    }
+    public void print() {
+        String str = "";
+        str += vertices + " vertices, " + edges + " edges" + "\n";
+        if (edges > 0) {
+            for (int i = 0; i < graphMatrix.length; i++) {
+                for (
+                    int j = 0; j < graphMatrix[0].length; j++) {
+                    str += graphMatrix[i][j] + " ";
+                }
+                str += "\n";
+            }
+            System.out.println(str);
+        } else {
+            System.out.println(str + "No edges");
+        }
+    }
+}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    
+    private Solution() {
+
+    }
+ 
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine();
+        switch (type) {
+        case "List":
+            ListGraph lgObj = new ListGraph(sc);
+            System.out.println(lgObj.toString());
+            break;
+        case "Matrix":
+            MatrixGraph mgObj = new MatrixGraph(sc);
+            mgObj.print();
+            break;
+        default:
+            break;
+        }
+    }
+}
