7fa14978c14bafeeba70a58b1a474a6258d06289
Madhu-92 <madhu.ktej@msitprogram.net>
2018-11-06 17:36:34 +0530

    "ads2_m4_a1_1: 2/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -2,9 +2,9 @@ public class Solution {
 
 	public static void main(String[] args) {
 		In inObj = new In();
-		String file1 = inObj.readString();
-		String file2 = inObj.readString();
-		String type = inObj.readString();
+		String file1 = inObj.readLine();
+		String file2 = inObj.readLine();
+		String type = inObj.readLine();
 		try {
 			if (type.equals("Graph")) {
 				WordNet wordnetObj = new WordNet(file1, file2);
