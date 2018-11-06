caa0cc735928a03ad590e439e02dc8e5c52add28
Madhu-92 <madhu.ktej@msitprogram.net>
2018-11-06 18:00:02 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -2,50 +2,50 @@
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		// Blank Constructor.
-	}
-	/**
-	 * Main method to perform operations.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		In inObj = new In();
-		String file1 = inObj.readLine();
-		String file2 = inObj.readLine();
-		String type = inObj.readLine();
-		try {
-			if (type.equals("Graph")) {
-				WordNet wordnet = new WordNet(file1, file2);
-				wordnet.display();
-				return;
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			if (type.equals("Queries")) {
-				while (!inObj.isEmpty()) {
-					String[] queryNames =
-					    inObj.readLine().split(" ");
-					if (queryNames[0].equals("null")) {
-						throw new Exception("IllegalArgumentException");
-					}
-					WordNet wordNetObj = new WordNet(file1, file2);
-					System.out.println("distance = "
-					                   + wordNetObj.distance(queryNames[0],
-					                           queryNames[1])
-					                   + ", ancestor = "
-					                   + wordNetObj.sap(queryNames[0],
-					                                    queryNames[1]));
-				}
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // Blank Constructor.
+    }
+    /**
+     * Main method to perform operations.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        In inObj = new In();
+        String file1 = inObj.readLine();
+        String file2 = inObj.readLine();
+        String type = inObj.readLine();
+        try {
+            if (type.equals("Graph")) {
+                WordNet wordnet = new WordNet(file1, file2);
+                wordnet.display();
+                return;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            if (type.equals("Queries")) {
+                while (!inObj.isEmpty()) {
+                    String[] queryNames =
+                        inObj.readLine().split(" ");
+                    if (queryNames[0].equals("null")) {
+                        throw new Exception("IllegalArgumentException");
+                    }
+                    WordNet wordNetObj = new WordNet(file1, file2);
+                    System.out.println("distance = "
+                                       + wordNetObj.distance(queryNames[0],
+                                               queryNames[1])
+                                       + ", ancestor = "
+                                       + wordNetObj.sap(queryNames[0],
+                                                        queryNames[1]));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
