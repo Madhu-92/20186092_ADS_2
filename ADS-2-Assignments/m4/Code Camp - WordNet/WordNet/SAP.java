e787d7b3dc1e46794fc2a19e9c86d72cbf322157
Madhu-92 <madhu.ktej@msitprogram.net>
2018-11-06 17:30:24 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/SAP.java
+++ ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,20 +1,41 @@
-/*public class SAP {
-
+public class SAP {
+    private Digraph graph;
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G)
+    private int distanceGlobal;
 
-    // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w)
-
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w)
-
+    public SAP(Digraph graph) {
+        this.graph = graph;
+        // distanceGlobal = 0;
+    }
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w)
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        ancestor(v, w);
+        // System.out.println(distanceGlobal);
+        return this.distanceGlobal;
+    }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(graph, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(graph, w);
+        this.distanceGlobal = Integer.MAX_VALUE;
+        int ancestors = -1;
+        for (int i = 0; i < graph.V(); i++) {
+            if (bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
+                int distance = bfsV.distTo(i) + bfsW.distTo(i);
+                if (distance < distanceGlobal) {
+                    //shortPath = distance;
+                    this.distanceGlobal = distance;
+                    // System.out.println("lkj");
+                    // System.out.println(distanceGlobal);
+                    ancestors = i;
+                }
+                // return ancestor;
+            }
+        }
+        return ancestors;
+    }
 
-    // do unit testing of this class
-    public static void main(String[] args)
-}*/
+    // // do unit testing of this class
+    // public static void main(String[] args)
+}
--- ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,14 +1,38 @@
 public class Solution {
 
-    public static void main(String[] args) {
-    	In inObj = new In();
-    	String file1 = inObj.readString();
-    	String file2 = inObj.readString();
-    	String type = inObj.readString();
-    	if (type.equals("Graph")) {
-    		WordNet wordnetObj = new WordNet(file1, file2);
-    	}
-        
-    }
-
+	public static void main(String[] args) {
+		In inObj = new In();
+		String file1 = inObj.readString();
+		String file2 = inObj.readString();
+		String type = inObj.readString();
+		try {
+			if (type.equals("Graph")) {
+				WordNet wordnetObj = new WordNet(file1, file2);
+				wordnetObj.display();
+				return;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			if (type.equals("Queries")) {
+				while (!inObj.isEmpty()) {
+					String[] queryNames =
+					    inObj.readLine().split(" ");
+					if (queryNames[0].equals("null")) {
+						throw new Exception("IllegalArgumentException");
+					}
+					WordNet wordNetObj = new WordNet(file1, file2);
+					System.out.println("distance = "
+					                   + wordNetObj.distance(queryNames[0],
+					                           queryNames[1])
+					                   + ", ancestor = "
+					                   + wordNetObj.sap(queryNames[0],
+					                                    queryNames[1]));
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
