59ce1e31c63642aa5d60f3ba6e47873d6c4b694c
Madhu-92 <madhu.ktej@msitprogram.net>
2018-12-20 11:47:27 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m3/3.2 Bipartite/Solution.java
+++ ADS-2-Assignments/m3/3.2 Bipartite/Solution.java
@@ -1,24 +1,105 @@
 import java.util.Scanner;
-public final class Solution {
-	protected Solution() {
+/**
+ * Class for bipartite.
+ */
+class Bipartite {
+    /**
+     * boolean array for marking the visit of node.
+     */
+    private boolean[] marked;
+    /**
+     * boolean array for color representation.
+     * of each node.
+     */
+    private boolean[] color;
+    /**
+     * flag to check graph is bipartite or not.
+     */
+    private boolean flag;
+    /**
+     * array for the edge of that node.
+     */
+    private int[] edgeTo;
+    /**
+     * Constructs the object.
+     * Time complexity is O(V)
+     * V is number of vertices.
+     *
+     * @param      g     graph object
+     */
+    Bipartite(final Graph g) {
+        int vertices = g.V();
+        marked = new boolean[vertices];
+        color = new boolean[vertices];
+        edgeTo = new int[vertices];
+        flag = true;
+        for (int i = 0; i < vertices; i++) {
+            if (!marked[i]) {
+                dfs(g, i);
+            }
+        }
+    }
+    /**
+     * this method performs dfs for the given graph.
+     * Time complexity is O(E)
+     * E is the number of edges.
+     *
+     * @param      g         graph object
+     * @param      vertices  The vertices
+     */
+    public void dfs(final Graph g, final int vertices) {
+        marked[vertices] = true;
+        for (int w : g.adj(vertices)) {
+            if (!flag) {
+                return;
+            } else if (!marked[w]) {
+                edgeTo[w] = vertices;
+                color[w] = !color[vertices];
+                dfs(g, w);
+            } else if (color[w] == color[vertices]) {
+                flag = false;
+            }
+        }
+    }
+    /**
+     * Determines if bipartite.
+     *
+     * @return     True if bipartite, False otherwise.
+     */
+    public boolean isBipartite() {
+        return flag;
+    }
+}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int numOfVertices = Integer.parseInt(sc.nextLine());
-		int numOfEdges = Integer.parseInt(sc.nextLine());
-		DiGraph dgObj = new DiGraph(numOfVertices);
-		while (numOfEdges > 0) {
-			String[] connectedVertices = sc.nextLine().split(" ");
-			dgObj.addEdge(Integer.parseInt(connectedVertices[0]),
-			           Integer.parseInt(connectedVertices[1]));
-			numOfEdges--;
-		}
-		DirectedCycle dcObj = new DirectedCycle(dgObj);
-		if (dcObj.isBipartite()) {
-			System.out.println("Graph is bipartite");
-		} else {
-			System.out.println("Graph is not a bipartite");
-		}
-	}
+    }
+    /**
+     * the main method performs operations.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        Graph g = new Graph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] t = sc.nextLine().split(" ");
+            g.addEdge(Integer.parseInt(
+                          t[0]), Integer.parseInt(t[1]));
+        }
+        Bipartite obj = new Bipartite(g);
+        if (obj.isBipartite()) {
+            System.out.println("Graph is bipartite");
+        } else {
+            System.out.println("Graph is not a bipartite");
+        }
+    }
 }
